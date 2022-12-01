038c807dac56c6e813dadbfd9f5d6d5aca772c9b
Mateusz Leo <leo.mateusz@gmail.com>
2022-12-01 23:03:11 +0100

    [day1] Solution

---
+++ day1/solution.py
@@ -0,0 +1,17 @@
+with open('./input.txt') as f:
+    xss = []
+    xs = []
+    while line := f.readline():
+        x = line.strip()
+        if not x:
+            xss.append(xs)
+            xs = []
+            continue
+        xs.append(int(x))
+    xs = [sum(xs) for xs in xss]
+    x = max(xs)
+    print('1', x)
+    xs = sorted(xs)[-3:]
+    x = sum(xs)
+    print('2', x)
+
